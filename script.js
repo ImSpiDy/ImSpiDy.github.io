76f496436c1e778a6f38cbbcf842e24657865ef3
ImSpiDy <SpiDy2713@gmail.com>
2022-09-18 18:19:00 +0530

    roll the dice
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-let joxx = ["What did the snail who was riding on the turtle's back say? Wheeeee!",
-"I was going to tell a time traveling joke, but you guys didn't like it.",
-"What do you call a lazy kangaroo? A pouch potato.",
-"I used to run a dating service for chickens, but I was struggling to make hens meet.",
-"Why was the coach yelling at the vending machine? He wanted his quarter back."]
+let digit = ["1",
+"2",
+"3",
+"4",
+"5",
+"6"]
 
 
-document.getElementsByClassName("joke").innerHTML = joxx[Math.floor(Math.random() * joxx.length)]
+document.getElementsByClassName("dice").innerHTML = digit[Math.floor(Math.random() * digit.length)]
 
 let btn = document.getElementById("btn");
 
 
-function jokk() {
-  let index = Math.floor(Math.random() * joxx.length)
-  document.getElementsByClassName("joke")[0].innerHTML = joxx[index]
+function xyz() {
+  let index = Math.floor(Math.random() * digit.length)
+  document.getElementsByClassName("dice")[0].innerHTML = digit[index]
 }
-btn.addEventListener("click", jokk)
+btn.addEventListener("click", xyz)
